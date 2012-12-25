892169ebe6dbe1244ebbefa3914f52f9c8ecd0af
Jeremy <herrmdawg@gmail.com>
2012-12-25 15:04:20 -0500

    Nav styling, notice div fix, tab header spacing

--- fsbky.js
+++ fsbky.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
     $('#headerDiv').load('Header.html');
-    $('#navigationDiv').load('Navigation.html');
+    $('#navigationDiv').load('Navigation.html', function() {
+        setSelectedNavItem();;
+      });
     $('#noticeDiv').load('Notice.html');
     if($("#tabs").length > 0){
         $( "#tabs" ).tabs();
     }
  });
+
+function setSelectedNavItem(){
+    if(document.URL.indexOf("index.html") > -1){
+        $('#navHome').addClass('selectedNavItem');
+    }
+    else if(document.URL.indexOf("Products.html") > -1){
+        $('#navProducts').addClass('selectedNavItem');
+    }
+    else if(document.URL.indexOf("CurrentLocations.html") > -1){
+        $('#navLocations').addClass('selectedNavItem');
+    }
+    else if(document.URL.indexOf("PrivacyPolicy.html") > -1){
+        $('#navPrivacy').addClass('selectedNavItem');
+    }
+    else if(document.URL.indexOf("Help.html") > -1){
+        $('#navHelp').addClass('selectedNavItem');
+    }
+    else if(document.URL.indexOf("Leadership.html") > -1){
+        $('#navLeadership').addClass('selectedNavItem');
+    }
+    else if(document.URL.indexOf("DialABank.html") > -1){
+        $('#navDial').addClass('selectedNavItem');
+    }
+    else if(document.URL.indexOf("Holidays.html") > -1){
+        $('#navHolidays').addClass('selectedNavItem');
+    }
+    else if(document.URL.indexOf("Specials.html") > -1){
+        $('#navSpecials').addClass('selectedNavItem');
+    }
+}
